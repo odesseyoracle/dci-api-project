48ac9d8624f1842ddd7c8df38312d9c2015a54ea
Jan <j.c.scherch@gmail.com>
2023-11-29 11:47:00 +0100

    Weather Test

--- script.js
+++ script.js
@@ -10,12 +10,16 @@ async function searchCountry(e) {
     const country = countryInput.value;
     const url = `https://restcountries.com/v3.1/name/${country}`;
 
+    const capital = data[0].capital;
+
     const response = await fetch(url);
     const data = await response.json();
     console.log(data);
 
+    getWeather();
+
     output.innerHTML = `<h2>${data[0].name.common} ${data[0].flag}</h2>
-    <p>Capital: ${data[0].capital}</p>
+    <p>Capital:<span id="capital"> ${data[0].capital}</span></p>
     <p>Region: ${data[0].region}</p>
     <p>Language : ${Object.values(data[0].languages)}</p>
     <p>Currency: ${Object.values(data[0].currencies)[0].name}</p>
@@ -23,4 +27,32 @@ async function searchCountry(e) {
     <a href="${data[0].maps.googleMaps}">Map</a>`;
     //
   } catch {}
+}
+
+async function getWeather() {
+  try {
+    const apiKey = "71f05fa9a67003870792a089b0de04e9";
+    const cityInput = capital;
+    console.log(cityInput);
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}&units=metric`;
+
+    const response = await fetch(weatherUrl);
+    const data = await response.json();
+    if (data.cod === "400") {
+      throw data;
+    }
+    console.log(data);
+
+    const iconUrl = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+
+    const result = `
+    <img src="${iconUrl}">
+    Tempratur in ${data.name}: ${data.main.temp} C, ${data.weather[0].description}
+    `;
+
+    document.getElementById("weather").innerHTML = result;
+  } catch (err) {
+    console.log(err);
+    document.getElementById("weather").textContent = err.message;
+  }
 }
